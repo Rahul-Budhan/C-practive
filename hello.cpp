1266af0fe8ed1c0eeaa2f05798ec418bb9eb9545
Rahul-budhan <rahulbudhan22@gmail.com>
2022-08-22 19:46:09 +0530

    added files

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int savings;
+    cin>>savings;
+
+    if(savings<5000)
+    {
+        cout<<"savings";
+    }
+    else
+    {
+        cout<<" savings";
+        }
+    return 0;
+}
---
+++ loops.cpp
@@ -0,0 +1,26 @@
+#include<iostream>;
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    cin>>a;
+    cin>>b;
+    for(int i=a;i<=b;i++)
+    {
+        if(i%2==0){
+            cout<<i<<" ";
+        }
+    }
+
+
+
+
+    // for(int i=1;i<=s;i++)
+    // {
+    //     sum=sum+i;
+    //         cout<<sum<<" ";
+    // }
+    
+    return 0;
+}
